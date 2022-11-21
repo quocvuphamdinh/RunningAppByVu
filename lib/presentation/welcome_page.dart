6e2d113ed6f4c3cec76b06028c71a78dc2d1a482
quocvuphamdinh <vuphamdinh02@gmail.com>
2022-11-21 21:08:25 +0700

    hi

---
+++ lib/presentation/welcome_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:running_app_flutter/presentation/widgets.dart';
+
+import '../utils/strings.dart';
+
+class VuHomePage extends StatelessWidget {
+  const VuHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        constraints: const BoxConstraints.expand(),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Image.asset(
+              "assets/images/runner.png",
+              height: 200,
+              width: 200,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(RunningAppStrings.titleApp,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "SecularOne")),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(5),
+            child: Text(
+              RunningAppStrings.subTitleApp,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: "SecularOne"),
+            ),
+          ),
+          Container(
+              margin: const EdgeInsets.only(top: 40),
+              child: buttonPrimary(
+                  context, RunningAppStrings.textCreateAccount, () {})),
+          Container(
+              margin: const EdgeInsets.only(top: 10),
+              child:
+                  buttonPrimary(context, RunningAppStrings.textSignIn, () {})),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  RunningAppStrings.subTitleApp2,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.grey[600],
+                      fontFamily: "SecularOne"),
+                ),
+              ),
+            ),
+          ),
+        ]),
+      )),
+    );
+  }
+}
---
+++ lib/utils/strings.dart
@@ -0,0 +1,8 @@
+class RunningAppStrings {
+  static const String titleApp = "Running App";
+  static const String subTitleApp = "by VuPham";
+  static const String textCreateAccount = "Create Account";
+  static const String textSignIn = "Sign In";
+  static const String subTitleApp2 =
+      "''One run can change your day, many runs can change your life.''";
+}
